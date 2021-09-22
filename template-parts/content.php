49c332edf6ee6dce263742cd1f29309f6d637f1e
anton <enyqyrypa@gmail.com>
2021-09-22 15:54:42 +0300

    news page

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,55 +9,93 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+<?php
+      if ( is_singular() ) :
+?>
+      <div class="container">
+        <div class="row">
+            <?php
+            the_title( '<h1 class="entry-title">', '</h1>' );
+            the_content(
+                sprintf(
+                    wp_kses(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'testwp' ),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    wp_kses_post( get_the_title() )
+                )
+            );
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				testwp_posted_on();
-				testwp_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+            wp_link_pages(
+                array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'testwp' ),
+                    'after'  => '</div>',
+                )
+            );
+            ?>
+        </div>
+      </div>
+      <?php
+      else :
+          ?>
+        <div class="col-4">
+          <div class="card" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+              <?php testwp_post_thumbnail(); ?>
+            <div class="card-body">
+                <?php
+                the_title( '<h5 class="card-title">', '</h5>' );
+                ?>
+              <p class="card-text">
+                  <?php
+                  the_content(
+                      sprintf(
+                          wp_kses(
+                          /* translators: %s: Name of current post. Only visible to screen readers */
+                              __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'testwp' ),
+                              array(
+                                  'span' => array(
+                                      'class' => array(),
+                                  ),
+                              )
+                          ),
+                          wp_kses_post( get_the_title() )
+                      )
+                  );
 
-	<?php testwp_post_thumbnail(); ?>
+                  wp_link_pages(
+                      array(
+                          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'testwp' ),
+                          'after'  => '</div>',
+                      )
+                  );
+                  ?>
+              </p>
+              <div>
+                  <?php
+                  the_field('news_date_field')
+                  ?>
+              </div>
+            </div>
+          </div><!-- #post-<?php the_ID(); ?> -->
+        </div>
+        <?php
+      endif;
+?>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'testwp' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'testwp' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php testwp_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+
+
+
+
+
+
+
+
+
+
